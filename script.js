29f2e18ba731f417c1f45046aa813c3a568f37fe
ArturMP <114751420+ArturMP@users.noreply.github.com>
2024-07-29 09:12:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ const perguntas = [
         enunciado: "Assim que saiu da escola você se depara com um grupo de jovens maltratando um animal silvestre, qual é a sua reação?",
         alternativas: [
             {
-                texto: "Eu ligaria para a policia ambiental e esperaria ela chegar para denuncia-los",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Eu ligaria para a policia ambiental e esperaria ela chegar para puni-los",
+                afirmacao: "No início pensou em ignorar a situação, para evitar problemas, porém decidiu fazer a coisa certa. "
             },
             {
                 texto: "Eu não me envolveria e passaria calado",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                afirmacao: "Por mais que a situação fosse ruim, fazer o bem seria muito arriscado para mim"
             }
         ]
     },
@@ -23,24 +23,24 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Eu deveria fazer a minha parte para tentar mudar essa situação e icentivar os outros a fazer o mesmo.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                afirmacao: "Diminuir a poluição é díficil,  porém você sabe que com pequenos esforços pode melhorar o meio ambiente."
             },
             {
                 texto: "Não há o que eu possa fazer, independente da boa ação, o mundo vai acabar um dia mesmo.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                afirmacao: "Você ficou desacreditado de que o mundo possa melhorar"
             }
         ]
     },
     {
-        enunciado: "Durante a sala de aula, a professora apresenta um projeto voluntário sobre a conscientização o descarte correto do lixo e recilcagem, e convida os alunos a participarem, o que você decide fazer",
+        enunciado: "Durante a sala de aula, a professora apresenta um projeto voluntário sobre a conscientização o descarte correto do lixo e reciclagem, e convida os alunos a participarem, o que você decide fazer",
         alternativas: [
             {
-                texto: "É uma boa ideia, mesmo não valendo nota, eu deveria participar para tornar o mundo um lugar menor",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "É uma boa ideia, mesmo não valendo nota, eu deveria participar para tornar o mundo um lugar melhor",
+                afirmacao: "Separar o lixo é trabalhoso, entretando você sabe que é o melhor para o meio ambiente"
             },
             {
                 texto: "Eu não vou particiar, preciso fazer outras coisas além disso",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                afirmacao: "As outras preocupações da vida te impedem de enxergar o futuro do planeta"
             }
         ]
     },
@@ -48,25 +48,25 @@ const perguntas = [
         enunciado: "Na escola, você esta andando com um amigo e ele joga um papel de bala no chão, você faz alguma coisa?",
         alternativas: [
             {
-                texto: "Eu corrigo ele e mando ele pegar o papel do chão para descartar no ligar correto",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Eu corrigo ele e peço para ele pegar o papel do chão para descartar no ligar correto",
+                afirmacao: "Para você, o correto é "lixo no lixo", se não tem um lixo proximo, segure-o até encontrar um local de descarte correto."
             },
             {
                 texto: "Eu não faço nada, um papel de bala não vai fazer diferença",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                afirmacao: "Para você, um pequeno papel não faz diferença, mas para o planeta faz."
             }
         ]
     },
     {
-        enunciado: "",
+        enunciado: "A temperatura está razoavel,e o ar-condicionado esta ligado, você olha e pensa",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Deveria desliga-lo para economizar um pouco de energia e aproveitar o clima natural",
+                afirmacao: "Pensando no própio consumo, você salvou um pouco do planeta para que as próximas gerações possam viver."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Vou deixar ligado, se eu tenho vou usar mesmo",
+                afirmacao: "Você pensa em si mesmo e não no impacto do meio ambiente, sendo assim, não aproveita o que a natureza pode te oferecer e impede as futuras gerações também."
             }
         ]
     },
